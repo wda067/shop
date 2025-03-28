a12a9aad6d31907979112752a0c17259ce9f0efb
Jo Myeong-geun <mg6359@naver.com>
2025-03-29 04:49:24 +0900

    feat: feign retryer 추가

--- src/main/java/com/shop/config/feign/TossPaymentsFeignConfig.java
+++ src/main/java/com/shop/config/feign/TossPaymentsFeignConfig.java
@@ -1,9 +1,11 @@
 package com.shop.config.feign;
 
 import feign.Logger;
+import feign.Retryer;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,7 +34,12 @@ public class TossPaymentsFeignConfig {
 
     @Bean
     public Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL; // 요청/응답 모든 로그 출력
+        return Logger.Level.FULL;  //요청 및 응답 모든 로그 출력
+    }
+
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(100, TimeUnit.MICROSECONDS.toMillis(500), 3);
     }
 }
 
