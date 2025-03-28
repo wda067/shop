e3072c980339b7b1f872117b39dfaa394de3c84c
Jo Myeong-geun <mg6359@naver.com>
2025-03-29 04:56:47 +0900

    refactor: dto 필드 수정

--- src/main/java/com/shop/dto/response/OrderResponse.java
+++ src/main/java/com/shop/dto/response/OrderResponse.java
@@ -8,15 +8,13 @@ public class OrderResponse {
 
     private final String email;
     private final String orderProduct;
-    private final String quantity;
     private final String totalAmount;
     private final String status;
 
     @Builder
-    public OrderResponse(String email, String orderProduct, String quantity, String totalAmount, String status) {
+    public OrderResponse(String email, String orderProduct, String totalAmount, String status) {
         this.email = email;
         this.orderProduct = orderProduct;
-        this.quantity = quantity;
         this.totalAmount = totalAmount;
         this.status = status;
     }
