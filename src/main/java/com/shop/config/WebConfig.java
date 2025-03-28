89fef1895e71149fcadf6b153e8e3066a05d8ca0
Jo Myeong-geun <mg6359@naver.com>
2025-03-29 05:49:33 +0900

    feat: cors 설정 추가

--- src/main/java/com/shop/config/WebConfig.java
+++ src/main/java/com/shop/config/WebConfig.java
@@ -3,8 +3,10 @@ package com.shop.config;
 import com.shop.config.auth.AuthArgumentResolver;
 import com.shop.config.auth.AuthInterceptor;
 import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,4 +29,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthArgumentResolver());
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost");
+            }
+        };
+    }
+
 }
