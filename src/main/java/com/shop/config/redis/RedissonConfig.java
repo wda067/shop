6f8a483ba3525691d4766386d3f2431c28fcedd4
Jo Myeong-geun <mg6359@naver.com>
2025-03-29 05:50:03 +0900

    feat: Redisson 설정 추가

---
+++ src/main/java/com/shop/config/redis/RedissonConfig.java
@@ -0,0 +1,27 @@
+package com.shop.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+@RequiredArgsConstructor
+public class RedissonConfig {
+
+    private final RedisProperties properties;
+
+    @Bean
+    public RedissonClient redisson() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + properties.getHost() + ":6379")
+                .setPassword(properties.getPassword());
+
+        return Redisson.create(config);
+    }
+}
