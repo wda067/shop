f6b16de9bdeb313454b2cd6f036d6ad9e5297e64
Jo Myeong-geun <mg6359@naver.com>
2025-03-29 04:44:00 +0900

    feat: @Async 설정 추가

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
+@EnableAsync
 public class ShopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shop/config/AsyncConfig.java
+++ src/main/java/com/shop/config/AsyncConfig.java
@@ -1,28 +1,23 @@
 package com.shop.config;
 
-import com.shop.config.mdc.MdcTaskDecorator;
-import com.shop.exception.CustomException;
 import java.util.concurrent.Executor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
 @Configuration
-@EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
-    @Bean(name = "asyncExecutor")
-    public Executor asyncExecutor() {
+    @Bean
+    public Executor emailTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
         executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("async-task-");
+        executor.setThreadNamePrefix("async-executor-");
 
         executor.setTaskDecorator(new MdcTaskDecorator());
         executor.initialize();
